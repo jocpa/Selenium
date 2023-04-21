fe0641a781d13bd0add12d9e5977d2a62be85993
Pablo Arzu <jptomas38@gmail.com>
2023-04-20 21:06:46 -0600

    Waits

---
+++ src/main/java/selenium/ImplicitWaint.java
@@ -0,0 +1,31 @@
+package selenium;
+import java.util.concurrent.TimeUnit;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ImplicitWaint {
+ 
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\jptom\\OneDrive\\Desktop\\Selenium-Java\\drivers\\chromedriver.exe");
+
+        //inicializar browser
+        WebDriver driver = new ChromeDriver();
+
+        //abrir pagina
+        driver.get("https://automationtesting.co.uk/loader.html");
+
+        //maximizar browser
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+         driver.findElement(By.cssSelector("#loaderBtn")).click();
+       
+        
+    }
+    
+}
