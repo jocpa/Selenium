96c43f49314eec02783653c92bc3df52b0f7f31e
Pablo Arzu <jptomas38@gmail.com>
2023-04-21 16:14:26 -0600

    Arrays.java added to java basics

---
+++ src/main/java/selenium/JavaBasics/Arrays.java
@@ -0,0 +1,16 @@
+package selenium.JavaBasics;
+
+public class Arrays {
+    
+    public static void main(String[] args) {
+
+        String[] months = {"june", "July", "August", "September"};
+        System.out.println(months[0]);
+
+        for(String month : months) {
+            System.out.println(month);
+        }
+
+    }
+    
+}
